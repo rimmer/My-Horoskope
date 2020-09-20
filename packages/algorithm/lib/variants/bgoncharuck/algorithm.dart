5dc1ff2fd49a24e9f30eba3d18a470aee7fc430c
Bohdan Honcharuk <dariydragon26@gmail.com>
2020-09-21 01:19:09 +0300

    algo: prophecies base value done (2/3)

--- packages/algorithm/lib/variants/bgoncharuck/algorithm.dart
+++ packages/algorithm/lib/variants/bgoncharuck/algorithm.dart
@@ -5,6 +5,7 @@ import 'package:prophecy_model/prophecy_model.dart';
 import 'package:users_repository/users_repository.dart';
 import 'package:userpoll/userpoll.dart';
 import 'package:algorithm/astro.dart';
+import 'methods_base.dart';
 import 'methods_userpollcalc.dart';
 import '../../interface.dart';
 import '../../astro.dart';
