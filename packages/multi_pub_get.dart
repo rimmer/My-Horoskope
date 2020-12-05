6fe517d8b2efb6180b8a9ccffdfa12a5ce09ab03
Igor Ianishevskyi <yarimmer@gmail.com>
2020-12-05 14:05:44 +0100

    Fixed pub get script, minor null check added

--- packages/multi_pub_get.dart
+++ packages/multi_pub_get.dart
@@ -1,11 +1,11 @@
 import 'dart:io';
 
 void main(List<String> args) {
-  final dir = args[0] ?? Directory.current;
+  final dir = args[0];
   new Directory(dir).listSync(recursive: true).forEach((entity) {
     if (entity is Directory) return;
 
-    final fileName = entity.path.split(Platform.pathSeparator)?.last;
+    final fileName = entity.path.split(Platform.pathSeparator).last;
     if (fileName == 'pubspec.yaml') {
       final path = entity.path.replaceFirst(fileName, '');
 
