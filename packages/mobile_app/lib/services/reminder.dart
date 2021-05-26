c21887151bd7d5047523a6a4304c33c86f891154
bgoncharuck <dariydragon26@gmail.com>
2021-05-26 21:13:46 +0300

    done, but need to fix predictions

--- packages/mobile_app/lib/services/reminder.dart
+++ packages/mobile_app/lib/services/reminder.dart
@@ -6,8 +6,8 @@ import 'local_notifications.dart';
 class _Remind {
   final Duration duration;
   final int id;
-  final String text;
-  const _Remind({this.duration, this.id, this.text});
+  final bool customText;
+  const _Remind({this.duration, this.id, this.customText = false});
 }
 
 Future reminderConfig() async {
@@ -15,13 +15,28 @@ Future reminderConfig() async {
   const reminders = <_Remind>[
     const _Remind(
       id: 1,
-      duration: Duration(seconds: 60),
-      text: "I am first",
+      duration: Duration(days: 3),
     ),
     const _Remind(
       id: 2,
-      duration: Duration(seconds: 120),
-      text: "I am second",
+      duration: Duration(days: 4),
+    ),
+    const _Remind(
+      id: 3,
+      duration: Duration(days: 5),
+    ),
+    const _Remind(
+      id: 4,
+      duration: Duration(days: 6),
+    ),
+    const _Remind(
+      id: 5,
+      duration: Duration(days: 13),
+    ),
+    const _Remind(
+      id: 6,
+      duration: Duration(days: 20),
+      customText: true,
     ),
   ];
 
@@ -35,11 +50,13 @@ Future reminderConfig() async {
   for (_Remind reminder in reminders)
     await timeoutNotification(
       channelInfo: Notif.reminderChannel,
-      timeout: reminder.duration,
-      body: reminder.text ??
-          await getPredictionText(
-            DateTime.now().add(reminder.duration).millisecondsSinceEpoch,
-          ),
+      // timeout: reminder.duration,
+      timeout: Duration(seconds: (reminder.id + 2) * 10),
+      body: reminder.customText == false
+          ? await getPredictionText(
+              DateTime.now().add(reminder.duration).millisecondsSinceEpoch,
+            )
+          : localeText.locale.youAreBusyNotif,
       eventId: reminder.id,
     );
 }
