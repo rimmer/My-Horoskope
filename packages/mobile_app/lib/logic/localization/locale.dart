c3c8505147b0126197dd30ed7504ac93b9ba9bcc
Igor Ianishevskyi <i.ianishevskyi@eyeo.com>
2021-09-19 16:03:40 +0200

    Choosing the right url with locale when open privacy policy or terms
    This commit fixes the bug with updates from older versions of the app, where the "none" locale is stored.

--- packages/mobile_app/lib/logic/localization/locale.dart
+++ packages/mobile_app/lib/logic/localization/locale.dart
@@ -2,26 +2,13 @@ import 'package:my_horoskope/app_global.dart';
 import 'package:text/text.dart';
 
 Future chooseLocale() async {
-  var languageCode = AppGlobal.data.appPref.locale.language;
-
-  switch (languageCode) {
-    case "ru":
-    case "be":
-    case "kk":
-    case "ky":
-    case "tg":
-    case "uz":
-    case "ce":
-    case "ab":
-    case "ro":
-    case "uk":
-      localeText.switchLocaleToRussian();
-      break;
-
-    case "en":
-    default:
-      localeText.switchLocaleToEnglish();
-      break;
+  // the right locale has already been set during cal to `guessLocaleFromSystem`
+  // hence here we have only "filtered" locales
+  // currently its only `ru` and `en`, which is the default one
+  if (AppGlobal.data.appPref.locale.language == "ru") {
+    localeText.switchLocaleToRussian();
+  } else {
+    localeText.switchLocaleToEnglish();
   }
 
   AppGlobal.data.predictions.jobSync("clear", null);
